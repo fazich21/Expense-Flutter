3223a2828ba3a6d3ee7cabe148fb144de25eb1a7
Faizan <134806934+fazich21@users.noreply.github.com>
2024-10-04 02:50:52 +0500

    Update Expense_item.dart

--- Expense_item.dart
+++ Expense_item.dart
@@ -1,22 +1,18 @@
-
 class ExpenseItem {
   final String name;
   final String amount;
   final DateTime dateTime;
-
-  ExpenseItem({
+ExpenseItem({
     required this.name,
     required this.amount,
     required this.dateTime,
   });
-
-  // Convert ExpenseItem to JSON
+ // Convert ExpenseItem to JSON
   Map<String, dynamic> toJson() => {
     'name': name,
     'amount': amount,
     'dateTime': dateTime.toIso8601String(),
   };
-
   // Convert JSON to ExpenseItem
   factory ExpenseItem.fromJson(Map<String, dynamic> json) {
     return ExpenseItem(
