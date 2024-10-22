0596b16ff18dd6596d7d96e758d81a09c102f96d
Faizan <134806934+fazich21@users.noreply.github.com>
2024-10-23 01:48:00 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'package:expanse/expense_data.dart';
+import 'package:expanse/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
+
+void main() async{
+  await Hive.initFlutter();
+
+  await Hive.openBox("expense_database");
+  runApp(Myapp());
+}
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    //SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return ChangeNotifierProvider(create: (context)=> ExpenseData(),
+    builder:(context, child)=>const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ) ,
+    );
+  }
+}
